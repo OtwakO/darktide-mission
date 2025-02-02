2298a4dd0b0753d33056a0eb3f99ae3247e840bd
OtwakO <tom28055101@gmail.com>
2025-02-03 07:07:16 +0800

    Fix backend

--- mission_fetcher.py
+++ mission_fetcher.py
@@ -18,108 +18,117 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 
 
 def refresh_token():
-    with open(Path("refresh_token.txt"), "r", encoding="utf-8") as f:
-        REFRESH_TOKEN = f.read().strip()
-    print("Fetching Access Token...")
-    url = "https://bsp-auth-prod.atoma.cloud/queue/refresh"
+    try:
+        with open(Path("refresh_token.txt"), "r", encoding="utf-8") as f:
+            REFRESH_TOKEN = f.read().strip()
+        url = "https://bsp-auth-prod.atoma.cloud/queue/refresh"
 
-    headers = {
-        "authorization": f"Bearer {REFRESH_TOKEN}",
-        "user-agent": USER_AGENT,
-    }
-    response = requests.get(url, headers=headers)
+        headers = {
+            "authorization": f"Bearer {REFRESH_TOKEN}",
+            "user-agent": USER_AGENT,
+        }
+        response = requests.get(url, headers=headers)
 
-    result = response.json()
-    new_token = result["AccessToken"]
-    auth_sub = result["Sub"]
-    with open(Path("refresh_token.txt"), "w", encoding="utf-8") as f:
-        f.write(result["RefreshToken"])
-    return new_token, auth_sub
+        result = response.json()
+        new_token = result["AccessToken"]
+        auth_sub = result["Sub"]
+        with open(Path("refresh_token.txt"), "w", encoding="utf-8") as f:
+            f.write(result["RefreshToken"])
+        return new_token, auth_sub
+    except Exception as e:
+        internal_notify(f"Failed to refresh token: {e}", sender="Mission Fetcher")
+        return None, None
 
 
 def fetch_missions(access_token):
-    print("Fetching Missions...")
-    url = "https://bsp-td-prod.atoma.cloud/mission-board"
-    headers = {
-        "authorization": f"Bearer {access_token}",
-        "user-agent": USER_AGENT,
-    }
-    response = requests.get(url, headers=headers)
-    return response.json()
+    try:
+        url = "https://bsp-td-prod.atoma.cloud/mission-board"
+        headers = {
+            "authorization": f"Bearer {access_token}",
+            "user-agent": USER_AGENT,
+        }
+        response = requests.get(url, headers=headers)
+        return response.json()
+    except Exception as e:
+        internal_notify(f"Failed to fetch missions: {e}", sender="Mission Fetcher")
+        return {}
 
 
 def parse_missions(missions_json):
-    print("Parsing Missions...")
-    missions = []
-    for mission in missions_json["missions"]:
-        # Check if map code has corresponding map name
-        map_name = MAPS.get(mission["map"], None)
-        if not map_name:
-            internal_notify(
-                f"Unknown map code: {mission['map']}", sender="Mission Fetcher"
+    try:
+        missions = []
+        for mission in missions_json["missions"]:
+            # Check if map code has corresponding map name
+            map_name = MAPS.get(mission["map"], None)
+            if not map_name:
+                internal_notify(
+                    f"Unknown map code: {mission['map']}", sender="Mission Fetcher"
+                )
+                map_name = mission["map"]
+            else:
+                map_name = MAPS.get(mission["map"])["en"]
+
+            # Check if map code has corresponding mission type
+            mission_type = MISSION_TYPES.get(mission["map"], None)
+            if not mission_type:
+                internal_notify(
+                    f"Unknown mission type for map: {mission['map']}",
+                    sender="Mission Fetcher",
+                )
+                mission_type = "Unknown"
+            else:
+                mission_type = MISSION_TYPES.get(mission["map"])["en"]
+
+            # Check if modifier code has corresponding modifiers then add all modifiers of all languages to keywords string
+            modifiers = MISSION_MODIFIERS.get(mission["circumstance"], None)
+            if not modifiers:
+                internal_notify(
+                    f"Unknown modifier code: {mission['circumstance']}",
+                    sender="Mission Fetcher",
+                )
+                modifiers = mission["circumstance"]
+                keywords = ""
+            else:
+                # keywords = " ".join(
+                #     [
+                #         value
+                #         for key, value in MISSION_MODIFIERS.get(
+                #             mission["circumstance"]
+                #         ).items()
+                #     ]
+                # )
+                keywords = MISSION_MODIFIERS.get(mission["circumstance"])["en"]
+
+            # Calculate experience and credits
+            experience = mission["xp"]
+            credits = mission["credits"]
+            for circumstance, bonus in mission["extraRewards"].items():
+                experience += bonus.get("xp", 0)
+                credits += bonus.get("credits", 0)
+
+            # Set Expiry Timestamp (Add 24 hours to epoch timestamp)
+            expiry_timestamp = int(mission["start"]) + (86400 * 1000)
+
+            mission_entry = Mission(
+                mission_id=mission["id"],
+                map_code=mission["map"],
+                map_name=map_name,
+                mission_type=mission_type,
+                mission_category=mission.get("category", "normal"),
+                challenge_level=f"challenge_level_0{mission['challenge']}",
+                side_mission=mission.get("sideMission", "no_side_mission"),
+                modifier_code=mission["circumstance"],
+                experience=experience,
+                credits=credits,
+                starting_timestamp=mission["start"],
+                expiring_timestamp=expiry_timestamp,
+                keywords=keywords,
             )
-            map_name = mission["map"]
-        else:
-            map_name = MAPS.get(mission["map"])["en"]
-
-        # Check if map code has corresponding mission type
-        mission_type = MISSION_TYPES.get(mission["map"], None)
-        if not mission_type:
-            internal_notify(
-                f"Unknown mission type for map: {mission['map']}",
-                sender="Mission Fetcher",
-            )
-            mission_type = "Unknown"
-        else:
-            mission_type = MISSION_TYPES.get(mission["map"])["en"]
-
-        # Check if modifier code has corresponding modifiers then add all modifiers of all languages to keywords string
-        modifiers = MISSION_MODIFIERS.get(mission["circumstance"], None)
-        if not modifiers:
-            internal_notify(
-                f"Unknown modifier code: {mission['circumstance']}",
-                sender="Mission Fetcher",
-            )
-            modifiers = mission["circumstance"]
-            keywords = ""
-        else:
-            # keywords = " ".join(
-            #     [
-            #         value
-            #         for key, value in MISSION_MODIFIERS.get(
-            #             mission["circumstance"]
-            #         ).items()
-            #     ]
-            # )
-            keywords = MISSION_MODIFIERS.get(mission["circumstance"])["en"]
-
-        # Calculate experience and credits
-        experience = mission["xp"]
-        credits = mission["credits"]
-        for circumstance, bonus in mission["extraRewards"].items():
-            experience += bonus.get("xp", 0)
-            credits += bonus.get("credits", 0)
-
-        # Set Expiry Timestamp (Add 24 hours to epoch timestamp)
-        expiry_timestamp = int(mission["start"]) + (86400 * 1000)
-
-        mission_entry = Mission(
-            mission_id=mission["id"],
-            map_code=mission["map"],
-            map_name=map_name,
-            mission_type=mission_type,
-            mission_category=mission.get("category", "normal"),
-            challenge_level=f"challenge_level_0{mission['challenge']}",
-            side_mission=mission.get("sideMission", "no_side_mission"),
-            modifier_code=mission["circumstance"],
-            experience=experience,
-            credits=credits,
-            starting_timestamp=mission["start"],
-            expiring_timestamp=expiry_timestamp,
-            keywords=keywords,
-        )
-        missions.append(mission_entry)
-    return missions
+            missions.append(mission_entry)
+        return missions
+    except Exception as e:
+        internal_notify(f"Failed to parse missions: {e}", sender="Mission Fetcher")
+        return []
 
 
 def add_fetched_missions_to_db(missions: list[Mission]):
@@ -129,14 +138,14 @@ def add_fetched_missions_to_db(missions: list[Mission]):
 
 
 async def update_mission_database():
-    print("Starting mission database fetching...")
+    print("Fetching Missions from Fatshark...")
     initialize_database()
     access_token, auth_sub = refresh_token()
     missions_json = fetch_missions(access_token)
     missions = parse_missions(missions_json)
     add_fetched_missions_to_db(missions)
     prune_expired_missions(int(time.time() * 1000))
-    print("Mission database fetched complete.")
+    print("Mission Fetched!")
 
 
 if __name__ == "__main__":
