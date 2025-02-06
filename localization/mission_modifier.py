ecede08734439d6050f50dd1f00554995eec8ff6
OtwakO <tom28055101@gmail.com>
2025-02-07 03:32:29 +0800

    Add high_flash_mission_16

--- localization/mission_modifier.py
+++ localization/mission_modifier.py
@@ -232,6 +232,11 @@ MISSION_MODIFIERS = {
         "zh-tw": "#高強度 #納垢賜福 #突擊兵部隊 #變種人 #冷卻時間減少 #額外爆炸桶",
         "zh-cn": "#高强度 #纳垢祝福 #突击兵部队 #变种人 #冷却时间减少 #额外爆炸桶",
     },
+    "high_flash_mission_16": {
+        "en": "#Hi-Intensity #Shock Troop #Mutants #Toxic Gas #Hunting Grounds",
+        "zh-tw": "#高強度 #突擊兵部隊 #變種人 #瘟疫毒氣 #狩獵場",
+        "zh-cn": "#高强度 #突击兵部队 #变种人 #瘟疫毒气 #狩猎场",
+    },
     "high_flash_mission_18": {
         "en": "#Hi-Intensity #Monstrous #Shock Troop #Sniper #Toxic Gas",
         "zh-tw": "#高強度 #怪物 #突擊兵部隊 #狙擊手 #瘟疫毒氣",
