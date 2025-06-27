a8a40a57466663c5b1123be00dc528b47e5708c5
OtwakO <tom28055101@gmail.com>
2025-06-27 09:40:55 +0800

    Update author's note

--- localization/authors_note.py
+++ localization/authors_note.py
@@ -1,11 +1,14 @@
 AUTHORS_NOTE = {
     "en": """
 ⚠️ Seems like Fatshark changed the missions to be expired pass the ~190 minute mark. (24hr -> ~190min)
+🚩 If you notice any unnamed modifiers, feel free to send me the exact modifiers in game so I can update it.
 """,
     "zh-tw": """
 ⚠️ 看起來肥鯊把任務改成大約190分鐘後就會過期。(24小時 -> 約190分鐘)
+🚩 如果你有注意到任何沒經過分類的詞墜名，歡迎隨時提供給我遊戲裡真實的詞墜去做更新，越詳細越好。
 """,
     "zh-cn": """
 ⚠️ 看起来肥鲨把任务改成大约190分钟后就会过期。(24小时 -> 约190分钟)
+🚩 如果你注意到任何没有分类的词条，欢迎随时提供给我游戏里的真实词条来做更新，越详细越好。
 """,
 }
